565140bf012c15a143ebd35fa52b57f8d756a4ca
Clara B <claratamsin5@gmail.com>
2023-08-15 13:24:40 -0400

    Initial Commit

---
+++ test github/test_githubApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_githubApp.swift
+//  test github
+//
+//  Created by Clara T Brown on 2023-08-15.
+//
+
+import SwiftUI
+
+@main
+struct test_githubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
